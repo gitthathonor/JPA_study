22606f666ff76e7878bcc9e5f613c9bafd19d3c6
gitthathonor <jeong.suyoung92@gmail.com>
2022-10-27 17:48:47 +0900

    통합 테스트 진행중 오류 발생

--- src/test/java/site/metacoding/white/web/UserApiControllerTest.java
+++ src/test/java/site/metacoding/white/web/UserApiControllerTest.java
@@ -1,5 +1,71 @@
 package site.metacoding.white.web;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+
+import site.metacoding.white.dto.UserReqDto.JoinReqDto;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class UserApiControllerTest {
+
+    // 테스트 시, Reflection을 통해서 DI
+    // @Autowired
+    // private UserService userService;
+
+    // spring에서 제공해주는
+    @Autowired
+    private TestRestTemplate rt;
+
+    private static ObjectMapper om;
+    private static HttpHeaders headers;
+
+    // 한 번만 띄우고 싶은 것들은 BeforeAll에다가 정의해놔야 한다.
+    @BeforeAll // static으로 메서드를 작성해야 한다.
+    public static void init() {
+        om = new ObjectMapper();
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Test
+    public void join_test() throws JsonProcessingException {
+        // given
+        JoinReqDto joinReqDto = new JoinReqDto();
+        joinReqDto.setUsername("hoho");
+        joinReqDto.setPassword("1234");
+        // 실제 컨트롤러는 json 데이터를 받기 때문에 json형태로 바꾸어야 한다.
+
+        String body = om.writeValueAsString(joinReqDto);
+        System.out.println(body);
+        // when
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        ResponseEntity<String> response = rt.exchange("/join", HttpMethod.POST, request, String.class);
+
+        // then
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getBody());
+
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        int code = dc.read("$.code");
+        Assertions.assertThat(code).isEqualTo(1);
+
+    }
 
 }
